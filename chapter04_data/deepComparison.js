528d337556126fe51bfb1fdcf56161fb42a902fd
Colin Mahoney <crm@selfmedicate.net>
2021-04-02 17:55:58 -0700

    add deepCompairson.js

--- chapter04_data/deepComparison.js
+++ chapter04_data/deepComparison.js
@@ -6,3 +6,36 @@ To find out whether values should be compared directly (use the === operator for
 The Object.keys function will be useful when you need to go over the properties of objects to compare them.
 */
 
+let deepEqual = (x, y) => {
+    if ((typeof(x) == 'object') && (typeof(y) == 'object')) {
+        if (x === null || y === null) {
+            if (x === null && y === null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (Object.keys(x).length == Object.keys(y).length) {
+            for (let key in x) {
+                if (!deepEqual(x[key], y[key])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    } else if (x === y) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
+
+let testObj1 = {a: 1, b: {ab: 1}};
+let testObj2 = {a: 1, c: {ab: 1}};
+
+console.log(deepEqual(testObj1, null));
+console.log(deepEqual(testObj1, testObj2));
+console.log(deepEqual(null, null));
+console.log(deepEqual(3, 3));
+console.log(deepEqual(3, "3"));
